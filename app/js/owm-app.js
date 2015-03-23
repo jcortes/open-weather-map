0e31fc4911dd44a263f61d7d5123bd7dac460f11
jcortes <jacortesmahmud@gmail.com>
2015-03-22 23:04:13 -0500

    unit 2 lesson 1 part 4

--- app/js/owm-app.js
+++ app/js/owm-app.js
@@ -1,21 +1,29 @@
-angular.module('owmApp', ['ngRoute'])
+angular.module('owmApp', ['ngRoute', 'ngAnimate'])
 
 .value('owmCities', ['New York', 'Dallas', 'Chicago'])
 
-.run(function($rootScope, $location){
+.run(function($rootScope, $location, $timeout){
     $rootScope.$on('$routeChangeError', function(){
         $location.path('/error');
+    });
+    $rootScope.$on('$routeChangeStart', function(){
+        $rootScope.isLoading = true;
+    });
+    $rootScope.$on('$routeChangeSuccess', function(){
+        $timeout(function() {
+            $rootScope.isLoading = false;
+        }, 1000);
     });
 })
 
 .config(['$routeProvider', function($routeProvider){
     $routeProvider.when('/', {
         templateUrl: 'home.html',
-        controller: 'HomeCtrl'
+        controller: 'HomeCtrl as hctrl'
     })
     .when('/cities/:city', {
         templateUrl: 'city.html',
-        controller: 'CityCtrl',
+        controller: 'CityCtrl as cctrl',
         resolve: {
             city: function(owmCities, $route, $location){
                 var city = $route.current.params.city;
@@ -37,7 +45,6 @@ angular.module('owmApp', ['ngRoute'])
     
 }])
 
-.controller('CityCtrl', ['$scope', 'city', function($scope, city){
-    var vm = $scope;
-    vm.city = city;
+.controller('CityCtrl', ['city', function(city){
+    this.city = city;
 }]);
